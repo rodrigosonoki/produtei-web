9038b382d1d00139f9439b706ac3e1784bd6b642
rodrigosonoki <rosonoki@gmail.com>
2022-02-22 22:51:37 -0300

    crossorigin to cross0rigin

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Banner from "../src/components/Banner";
 import Team from "../src/components/Team";
-
 import Header from "../src/components/Header";
 import RotatingBanner from "../src/components/RotatingBanner";
 import Footer from "../src/components/Footer";
@@ -13,7 +12,7 @@ export default function Home() {
         <title>Produtei</title>
         <meta name="description" content="Produtei" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" cross0rigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700;900&family=Patua+One&display=swap"
           rel="stylesheet"
